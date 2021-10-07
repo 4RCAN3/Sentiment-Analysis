be615da5400326e525416ab42f2812019fe52fb5
Ayush Katoch <katochayushhp@gmail.com>
2021-10-07 12:07:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const moment = require("moment");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const ejs = require("ejs")
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose")
+const ejs = require("ejs");
+
+const today = moment();
+
+let currentDay =   today.format('YYYY-MM-DD')
 
 
 const app = express();
+
+app.use(
+  session({
+    secret: "My name is Ayush",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +31,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/someDB", { useNewUrlParser: true });
+
 app.get("/", (req, res) => {
-    res.render("sentiment")
+  
+    res.render("sentiment", {currentDay : currentDay })
 })
 
 app.listen(5000, () => {
